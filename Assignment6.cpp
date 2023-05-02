9220ea5755fae5cf7a975dfad11451ee8016fa7d
sejin8839 <sejinon01@gmail.com>
2023-05-02 17:40:36 +0900

    Update Assignment6.cpp

--- Assignment6.cpp
+++ Assignment6.cpp
@@ -1,3 +1,9 @@
+/*
+Author - Name:Sejin Yoon ID:u1311019
+Date: May 2, 2023
+filename: Assignment6.cpp
+*/
+
 #include<iostream>
 #include<cmath>
 #include<iomanip>
@@ -12,14 +18,19 @@ float Distance(const Vertex &v1 ,const Vertex &v2)
 {
   
     
-    //Student Todos ...
+    return sqrt(pow(v2.x - v1.x, 2) + pow(v2.y - v1.y, 2));
 
 }
 
 float triangleArea(const Vertex &v1, const Vertex &v2, const Vertex &v3)
 {
 
-   //Student Todos ...
+    float a = Distance(v1, v2);
+    float b = Distance(v2, v3);
+    float c = Distance(v3, v1);
+
+    float s = (a + b + c) / 2.0;
+    return sqrt(s * (s - a) * (s - b) * (s - c));
 
 }
 int main()
@@ -33,5 +44,8 @@ int main()
     cout<< "Enter the third vertex coordinates of the triangle: ";
     std::cin>>v3.x>>v3.y;
 
-    //Student Todos ...
+    float area = triangleArea(v1, v2, v3);
+    cout << "The area of the triangle is: " << std::fixed << std::setprecision(2) << area << endl;
+
+    return 0;
 }
