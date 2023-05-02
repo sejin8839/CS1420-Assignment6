84fd1672fbd72ae555cf155c87ef4c5afde227f0
sejin8839 <sejinon01@gmail.com>
2023-05-02 17:34:57 +0900

    Create Assignment6.cpp

---
+++ Assignment6.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+struct Vertex
+{
+    float x,y;
+}; 
+
+float Distance(const Vertex &v1 ,const Vertex &v2)
+{
+  
+    
+    //Student Todos ...
+
+}
+
+float triangleArea(const Vertex &v1, const Vertex &v2, const Vertex &v3)
+{
+
+   //Student Todos ...
+
+}
+int main()
+{
+    Vertex v1,v2,v3;
+
+    cout<< "Enter the first vertex coordinates of the triangle: ";
+    std::cin>>v1.x>>v1.y;
+    cout<< "Enter the second vertex coordinates of the triangle: ";
+    std::cin>>v2.x>>v2.y;
+    cout<< "Enter the third vertex coordinates of the triangle: ";
+    std::cin>>v3.x>>v3.y;
+
+    //Student Todos ...
+}
